51f0a8873654fff9816990d11a31ad77070da99f
Shubh0405 <shubhngupta04@gmail.com>
2020-08-04 20:17:52 +0530

    Javascript base code added

--- starter/app.js
+++ starter/app.js
@@ -8,3 +8,67 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+
+  // 1. Random Numbers for dice.
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result.
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  //3. Update the round score if the rolled number was not 1.
+  if(dice !== 1){
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }else{
+    nextPlayer();
+  }
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+  // Add Current score to global score.
+  scores[activePlayer] += roundScore;
+
+  // Update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+
+  // Check if player won the game.
+  if(scores[activePlayer] >= 20){
+    document.querySelector('#name-'+activePlayer).textContent = 'WINNER!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  }else{
+    nextPlayer();
+  }
+
+});
+
+function nextPlayer(){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+};
