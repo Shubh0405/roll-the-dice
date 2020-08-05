4ab65fe69164cff7188e21ef93f5ad8868015312
Shubh0405 <shubhngupta04@gmail.com>
2020-08-06 01:58:36 +0530

    final score 100

--- starter/app.js
+++ starter/app.js
@@ -47,7 +47,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
 
 
     // Check if player won the game.
-    if(scores[activePlayer] >= 20){
+    if(scores[activePlayer] >= 100){
       document.querySelector('#name-'+activePlayer).textContent = 'WINNER!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
